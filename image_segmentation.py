4d5236a3dc98c43ad914cfd8c86d9a4920c99a2e
jili5044 <jili5044@colorado.edu>
2022-11-01 17:27:42 +0000

    image segmentation

--- image_segmentation.py
+++ image_segmentation.py
@@ -4,31 +4,34 @@ from matplotlib import pyplot as plt
 
 # img = cv2.imread('coins.png')
 img = cv2.imread('sic_pre_1-slice2-adjusted.tif')
+# img = cv2.imread('ex11.tif')
+
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
+im_blur = cv2.GaussianBlur(gray,(15,15),0)
+ret, thresh = cv2.threshold(im_blur,135,255,1)
+new_thresh = cv2.bitwise_not(thresh)
 
 # noise removal
 kernel = np.ones((2,2),np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 1)
-cv2.imshow("opening", opening)
+opening = cv2.morphologyEx(new_thresh,cv2.MORPH_OPEN,kernel, iterations = 1)
 
 
 # sure background area
-sure_bg = cv2.dilate(opening,kernel,iterations=9)
+sure_bg = cv2.dilate(opening,kernel,iterations=8)
+# cv2.imshow("sure_bg", sure_bg)
 
 
 # Finding sure foreground area
-dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-ret, sure_fg = cv2.threshold(dist_transform,0.1*dist_transform.min(),255,0)
+dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,3)
+ret, sure_fg = cv2.threshold(dist_transform,0.35*dist_transform.max(),255,0)
+cv2.imshow("opening", opening)
 
 
 # Finding unknown region
 sure_fg = np.uint8(sure_fg)
 unknown = cv2.subtract(sure_bg,sure_fg)
-
-
+cv2.imshow("unknown", unknown)
 
 
 # Marker labelling
