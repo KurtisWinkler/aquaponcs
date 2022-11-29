4f9d8e1c5e1831d3df6d9bb93384afa973edbb67
KurtisWinkler <111379483+KurtisWinkler@users.noreply.github.com>
2022-11-29 13:17:23 -0700

    update docs and pycodestyle

--- blob_class.py
+++ blob_class.py
@@ -11,9 +11,9 @@ import matplotlib.pyplot as plt
 class Blob(RegionProperties):
     """
     A class to get attributes of Blobs
-    
+
     Description:
-    Inherits from the RegionProperties class in skimage.
+    Inherits from the RegionProperties class in scikit-image.
     List of properties that can be obtained are found in
     regionprops documentation:
     "https://scikit-image.org/docs/stable/api/
@@ -23,9 +23,11 @@ class Blob(RegionProperties):
     ----------
     contour : nested list
         contains contour points of blob
-        
+        scikit format of contours
+
     cv_contour : nested list
         same as contour, but with points in double brackets
+        opencv format of contours
 
     orig_image : image matrix
         original image that contour was made from
@@ -97,45 +99,44 @@ class Blob(RegionProperties):
     roundness
     *slice
     *solidity
-
     """
 
     def __init__(self, contour, orig_image):
 
         if not isinstance(contour, (list, np.ndarray)):
             raise TypeError('contour must be list or numpy array')
-            
+
         if not isinstance(orig_image, np.ndarray):
             raise TypeError('orig_image must be a numpy array')
-        
+
         contour = np.array(contour)
-        
+
         # if opencv contour, remove double brackets
         if len(contour.shape) == 3 and contour.shape[1] == 1:
             self.cv_contour = contour.copy()
-            contour = contour[:,0]
+            contour = contour[:, 0]
         elif contour.shape == 2:
             self.cv_contour = np.array([[pt] for pt in contour])
 
         self.contour = contour
         self.orig_image = orig_image
-        
+
         self.label_im = label(self.image_mask)
-        
+
         sl = ndi.find_objects(self.label_im)
-        super().__init__(slice = sl[0],  # take only slice from list
-                         label = 1,  # only one slice
-                         label_image = self.label_im,
-                         intensity_image = orig_image,
-                         cache_active = False)
-    
+        super().__init__(slice=sl[0],  # take only slice from list
+                         label=1,  # only one slice
+                         label_image=self.label_im,
+                         intensity_image=orig_image,
+                         cache_active=False)
+
     # ALWAYS USE area_filled and perimeter_crofton for accurate results
-    
+
     @property
     def aspect_ratio(self):
         """Return aspect ratio as float"""
         return self.axis_major_length / self.axis_minor_length
-    
+
     @property
     def centroid_xy(self):
         """Return centroid as tuple of numpy.float64"""
@@ -153,16 +154,40 @@ class Blob(RegionProperties):
         return min(circularity, 1)
 
     def curvature(self, num_space=5):
-        """Return curvature at each contour point as numpy.ndarray"""
-        
+        """
+        Return curvature at each contour point as numpy.ndarray
+
+        Parameters
+        ----------
+        num_space : list of floats
+            how far left/right points will be for calculating gradient
+        """
+
         def gradient_spaced(L, num):
-            grad = np.array([(L[i+num] - L[i-num])/(num*2) for i in range(-num,len(L)-num)])
+            """
+            Returns gradient calculated at each point in list
+
+            Parameters
+            ----------
+            L : list of floats
+                gradient will be calculated on these points
+
+            num : int
+                how far left/right points will be for calculating gradient
+
+            Returns
+            -------
+            grad : numpy.ndarray
+                array of gradients calculated at each point
+            """
+            grad = np.array([(L[i+num] - L[i-num])/(num*2)
+                             for i in range(-num, len(L)-num)])
             # reorder matrix to align with contour indices
             grad = np.append(grad[num:], grad[0:num])
             return grad
 
         contour = self.contour
-        
+
         dx_dt = gradient_spaced(contour[:, 0], num_space)
         dy_dt = gradient_spaced(contour[:, 1], num_space)
 
@@ -179,26 +204,27 @@ class Blob(RegionProperties):
         d2x_dt2 = gradient_spaced(dx_dt, num_space)
         d2y_dt2 = gradient_spaced(dy_dt, num_space)
 
-        curvature = np.abs(d2x_dt2 * dy_dt - dx_dt * d2y_dt2) / (dx_dt * dx_dt + dy_dt * dy_dt)**1.5
+        curvature = np.abs(d2x_dt2 * dy_dt - dx_dt * d2y_dt2) / \
+                          (dx_dt * dx_dt + dy_dt * dy_dt)**1.5
 
         return curvature
 
     def curvature_mean(self, num=5):
         """Return mean of curvature as numpy.float64"""
         return np.mean(self.curvature(num))
-    
+
     @property
     def ellipse_fit_residual(self):
-        """ 
+        """
         Returns residual of ellipse fit at each contour point as numpy.ndarray
-        
-        Fits the contour to an ellipse, then returns the mean
-        residuals (shortest distance of contour point to 
-        ellipse model)
-            
+
+        Fits the contour to an ellipse, then calculates
+        residuals (shortest distance of contour point to
+        ellipse fit model)
+
         This estimates how well the contour fits an ellipse model
         """
-        
+
         contour = self.contour
         ellipse = EllipseModel()
 
@@ -208,7 +234,7 @@ class Blob(RegionProperties):
             return residuals
         else:
             return None
-        
+
     @property
     def ellipse_fit_residual_mean(self):
         """Return mean of residuals as numpy.float64"""
@@ -224,38 +250,39 @@ class Blob(RegionProperties):
         im = self.image_convex.astype(np.uint8)
         im[im == 1] = 255
         return im
-    
+
     @property
     def image_mask(self):
         """Return image with contour as mask as numpy.ndarray"""
         mask = np.zeros(self.orig_image.shape[0:2], np.uint8)
         cv.fillPoly(mask, pts=[self.contour], color=(255, 255, 255))
         return mask
-    
+
     @property
     def image_mask_bbox(self):
         """Return image with contour as mask in bbox as numpy.ndarray"""
         im = self.image.astype(np.uint8)
         im[im == 1] = 255
         return im
-    
+
     @property
     def image_masked(self):
         """Return original image with contour as mask as numpy.ndarray"""
         return cv.bitwise_and(self.orig_image,
                               self.orig_image,
                               mask=self.image_mask)
-    
+
     @property
     def perimeter_convex_hull(self):
         """Return perimeter of convex hull as numpy.float64"""
         convex_label = label(self.image_convex)
         convex_perimeter = regionprops(convex_label)[0]['perimeter_crofton']
         return max(convex_perimeter, 1)
-    
+
     @property
     def pixel_intensities(self):
-        """Return grayscale pixel intensites of area inside contour as numpy.ndarray"""
+        """Return grayscale pixel intensites of
+            area inside contour as numpy.ndarray"""
         coords = np.where(self.image_mask == 255)
         if self.orig_image.ndim > 2:
             image_gray = cv.cvtColor(self.image_masked, cv.COLOR_BGR2GRAY)
@@ -271,7 +298,7 @@ class Blob(RegionProperties):
     def pixel_intensity_median(self):
         """Return median of pixel intensities as numpy.float64"""
         return np.median(self.pixel_intensities)
-    
+
     def pixel_intensity_percentile(self, percentile=75):
         """Return pixel intensity of specified percentile as numpy.uint8"""
         pixel_sort = np.sort(self.pixel_intensities)
@@ -292,14 +319,14 @@ class Blob(RegionProperties):
     def pixel_skew(self):
         """Return skew of pixel intensities as float"""
         return skew(self.pixel_intensities, bias=False, nan_policy='omit')
-    
+
     @property
     def roughness_perimeter(self):
         """Return perimeter roughness as numpy.float64"""
         roughness = self.perimeter_crofton / self.perimeter_convex_hull
         # perimeter roughness cannot be less than 1 (rounding errors)
         return max(roughness, 1)
-    
+
     @property
     def roughness_surface(self):
         """Return surface roughness as numpy.float64"""
@@ -314,7 +341,7 @@ class Blob(RegionProperties):
         num = 4 * self.area_filled
         den = math.pi * pow(self.axis_major_length, 2)
         return num / den
-    
+
     def print_properties(self, dec=2):
         funcs = [
             'aspect_ratio',
@@ -346,10 +373,9 @@ class Blob(RegionProperties):
             try:
                 val = eval('self.' + funcs[i])
                 print(funcs[i] + ': ' + str(np.around(val, dec)))
-            except:
+            except Exception as e:
                 val = eval('self.' + funcs[i] + '()')
                 print(funcs[i] + ': ' + str(np.around(val, dec)))
-
 
 
 def plot_image(blob):
@@ -392,6 +418,7 @@ def main():
     plt.hist(blob.pixel_intensities,256,[0,256]); plt.show()
     cv.waitKey()
     '''
-    
+
+
 if __name__ == '__main__':
     main()
