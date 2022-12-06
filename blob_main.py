f5d233c99fe762420c69e5b9f42eb5f0502eafc4
KurtisWinkler <111379483+KurtisWinkler@users.noreply.github.com>
2022-12-05 20:06:50 -0700

    add args for command line

--- blob_main.py
+++ blob_main.py
@@ -1,12 +1,43 @@
 import cv2 as cv
 import pandas as pd
+import blob_args as ba
 import blob_class as bc
 import blob_detection as bd
 import contrast_functions as cf
 import param_output as po
 
+# get input arguments
+args = ba.get_args()
+
+# create initial filters for blobs
+if args.init_filter is None:
+    init_filter = [['area', 20, None],
+                   ['ellipse_fit_residual_mean', None, 1],
+                   ['pixel_kurtosis', None, 0]]
+else:
+    init_filter = args.init_filter
+    
+# create filter to get blobs most simliar to each other
+if args.sim_filter is None:
+    sim_filter = [['pixel_intensity_percentile(10)', 0.2]]
+else:
+    sim_filter = args.sim_filter
+
+# create filter to remove outlier blobs
+if args.out_filter is None:
+    out_filter = [['pixel_intensity_percentile(10)', 0.2, 1]]
+else:
+    out_filter = args.out_filter
+
+# create criteria for determining best blob
+if args.best_filter is None:
+    best_filter = [['area_filled', 'max', 1],
+                   ['roughness_surface', 'min', 1]]
+else:
+    best_filter = args.best_filter
+
 # read in image and save
-im = cv.imread('ex1.tif')
+im = cv.imread(args.file_name)
 cv.imwrite('1_original_image.jpg', im)
 
 # scale image so that at least 0.35% of pixels
@@ -33,7 +64,9 @@ contour = max(contours, key=cv.contourArea)
 nuc_blur = bc.Blob(contour, im_blur)
 
 # find maxima in nucleus
-local_max_coords = bd.get_maxima(nuc_blur, 5, 0.8)
+local_max_coords = bd.get_maxima(image=nuc_blur,
+                                 distance=args.min_distance,
+                                 threshold=args.min_thresh_maxima)
 
 # create and save image showing maxima
 im_maxima = im_scaled.copy()
@@ -42,59 +75,58 @@ for coordinate in local_max_coords:
 cv.imwrite('4_peak_local_maxima.jpg', im_maxima)
 
 # set minimum threshold to find contours
-min_thresh = int(nuc_blur.pixel_intensity_percentile(80))
+min_thresh = int(nuc_blur.pixel_intensity_percentile(
+                 args.min_thresh_contours * 100))
 
 # find contours
-contours = bd.get_contours(im_blur, min_thresh, thresh_step=3)
+contours = bd.get_contours(image=im_blur,
+                           thresh_min=min_thresh,
+                           thresh_step=args.thresh_step)
 
 # get list of blobs organized by maxima
 contour_list = bd.organize_contours(contours,
                                     local_max_coords,
                                     im_blur,
-                                    min_distance=5)
+                                    min_distance=args.min_distance)
+# remove empty lists
+contour_list = [contour for contour in contour_list if contour]
 
 # save image with all blob contours
 cv.imwrite('5_maxima_blobs.jpg', bd.blob_im(im_scaled, contour_list))
 
-# create filters for blob
-filters = [['area', 10, None],
-           ['ellipse_fit_residual_mean', None, 1],
-           ['pixel_kurtosis', None, 0]]
-
 # filter blobs based on filters list
-blob_list = []
-for contours in contour_list:
-    blob_list.append([x for x in contours if bd.blob_filter(x, filters)])
+if args.no_init_filter is False:
+    blob_list = []
+    for contours in contour_list:
+        blob_list.append([x for x in contours if bd.blob_filter(x, init_filter)])
 
-# save image with filtered contours
-cv.imwrite('6_filtered_blob.jpg', bd.blob_im(im_scaled, blob_list))
-
-# create params to get blobs most simliar to each other
-params = [['pixel_intensity_percentile(10)', 0.2]]
+    # save image with filtered contours
+    cv.imwrite('6_filtered_blob.jpg', bd.blob_im(im_scaled, blob_list))
+else:
+    blob_list = contour_list
 
 # filter blobs based on similarity
-sim_blobs = [bd.similar_filter(blobs, params, 2) for blobs in blob_list]
-sim_blobs = [blobs for blobs in sim_blobs if blobs is not None]
+if args.no_sim_filter is False:
+    sim_blobs = [bd.similar_filter(blobs, sim_filter, 2) for blobs in blob_list]
+    sim_blobs = [blobs for blobs in sim_blobs if blobs is not None]
 
-# save image with most similar blobs contours for each unique_point
-cv.imwrite('7_similar_blobs.jpg', bd.blob_im(im_scaled, sim_blobs))
-
-# create out_filter list to filter out outlier blobs
-out_filter = [['pixel_intensity_percentile(10)', 0.2, 1]]
-
+    # save image with most similar blobs contours for each unique_point
+    cv.imwrite('7_similar_blobs.jpg', bd.blob_im(im_scaled, sim_blobs))
+else:
+    sim_blobs = blob_list
+    
 # filter out outliers
-no_outs = [bd.outlier_filter(blobs, out_filter) for blobs in sim_blobs]
+if args.no_out_filter is False:
+    no_outs = [bd.outlier_filter(blobs, out_filter) for blobs in sim_blobs]
 
-# save image with blobs that are not outliers
-cv.imwrite('8_no_outlier_blobs.jpg', bd.blob_im(im_scaled, no_outs))
-
-# create criteria for determining best blob
-criteria = [['area_filled', 'max', 1],
-            ['roughness_surface', 'min', 1]]
+    # save image with blobs that are not outliers
+    cv.imwrite('8_no_outlier_blobs.jpg', bd.blob_im(im_scaled, no_outs))
+else:
+    no_outs = sim_blobs
 
 # get blob with highest score for each unique_point
-blobs_best = [bd.blob_best(blobs, criteria) for blobs in no_outs]
-
+blobs_best = [bd.blob_best(blobs, best_filter) for blobs in no_outs]
+print(blobs_best)
 # save image with best blobs for each unique_point
 cv.imwrite('9_best_blobs.jpg', bd.blob_im(im_scaled, blobs_best))
 
