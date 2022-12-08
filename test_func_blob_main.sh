96e475d4a3dd29fc383dcfe68be4cb117793e070
KurtisWinkler <111379483+KurtisWinkler@users.noreply.github.com>
2022-12-07 20:34:27 -0700

    create file

---
+++ test_func_blob_main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+run test_blob_main blob_main.py --file_name 'ex1.tif'
+assert_no_stdout
+assert_exit_code 0
