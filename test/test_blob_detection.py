5b68081f7b91b32a5cf9bc2229e35fb4e93863cc
Henry Waldstreicher <hjwaldstreicher@gmail.comm>
2022-12-08 21:26:06 +0000

    added import sys for correct pathing

--- test/test_blob_detection.py
+++ test/test_blob_detection.py
@@ -2,6 +2,8 @@ import unittest
 import numpy as np
 import cv2 as cv
 from scipy import ndimage as ndi
+import os
+import sys
 src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 sys.path.append(src_path)
 import blob_class as bc
