ab8059eb77c29a3d13733cc1b4255f3181f32885
KurtisWinkler <111379483+KurtisWinkler@users.noreply.github.com>
2022-12-08 12:11:56 -0700

    updated file location

--- test/test_func_blob_main.sh
+++ test/test_func_blob_main.sh
@@ -3,6 +3,6 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-run test_blob_main python ../blob_main.py --file_name 'ex1.tif'
+run test_blob_main python ../blob_main.py --file_name 'example_images/ex1.tif'
 assert_no_stdout
 assert_exit_code 0
