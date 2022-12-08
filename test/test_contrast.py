32cf6b6bb12ab15f7c78ff791c028a52e272e535
Henry Waldstreicher <hjwaldstreicher@gmail.comm>
2022-12-08 19:25:22 +0000

    edited an import statement and edited for pycodestyle

--- test/test_contrast.py
+++ test/test_contrast.py
@@ -1,32 +1,23 @@
 import unittest
-import contrast_function as cf
+import contrast_functions as cf
 import os
 import sys
 
-### UNIT TESTS ###
 
 class classTestContrast(unittest.TestCase):
-    # setUp and tearDown
-#     @classmethod
-#     def setUpClass(cls):
-        # not sure class variables are necessary here
-    
-#     @classmethod
-#     def tearDownClass(cls):
-        
-    
+
     def test_min_max_rescale(self):
-        # test to make sure the file exists 
-        if cf.min_max_rescale('bad_file_path.csv'):        
+        # test to make sure the file exists
+        if cf.min_max_rescale('bad_file_path.csv'):
             self.assertRaise(FileNotFoundError)
-        
-        
+
     def test_percentile_rescale(self):
         # test to make sure the file exists
-        if cf.percentile_rescale('bad_file_path.csv'):        
+        if cf.percentile_rescale('bad_file_path.csv'):
             self.assertRaise(FileNotFoundError)
-        
-        # want a positive test for min and max percentiles     
-        
+
+        # want a positive test for min and max percentiles
+
+
 if __name__ == '__main__':
     unittest.main()
