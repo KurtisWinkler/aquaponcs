b04374574db3dc85495f3dc76a51847a1d2d7136
Henry Waldstreicher <hjwaldstreicher@gmail.comm>
2022-12-08 21:21:21 +0000

    edited pathing for imports

--- test/test_blob_class.py
+++ test/test_blob_class.py
@@ -4,6 +4,7 @@ from scipy import ndimage as ndi
 from scipy.stats import skew, kurtosis
 import math
 import cv2 as cv
+import os
 src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 sys.path.append(src_path)
 import blob_class as bc
