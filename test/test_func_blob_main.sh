baa5b281027666b96743843118388bd53ac18faa
KurtisWinkler <111379483+KurtisWinkler@users.noreply.github.com>
2022-12-09 17:04:33 -0700

    update tests

--- test/test_func_blob_main.sh
+++ test/test_func_blob_main.sh
@@ -7,7 +7,7 @@ run test_blob_main1 python ../blob_main.py --file_name '../example_images/ex1.ti
 assert_no_stdout
 assert_exit_code 0
 
-run test_blob_main2 python ../blob_main.py --file_name '../example_images/ex1.tif' --min_distance 3
+run test_blob_main2 python ../blob_main.py --file_name '../example_images/ex1.tif' --min_distance 20
 assert_no_stdout
 assert_exit_code 0
 
@@ -20,22 +20,6 @@ assert_no_stdout
 assert_exit_code 0
 
 run test_blob_main5 python ../blob_main.py --file_name '../example_images/ex1.tif' --thresh_step 3
-assert_no_stdout
-assert_exit_code 0
-
-run test_blob_main6 python ../blob_main.py --file_name '../example_images/ex1.tif' --init_filter '[['area', 20, None]]'
-assert_no_stdout
-assert_exit_code 0
-
-run test_blob_main7 python ../blob_main.py --file_name '../example_images/ex1.tif' --sim_filter '[['perimeter', 0.2]]'
-assert_no_stdout
-assert_exit_code 0
-
-run test_blob_main8 python ../blob_main.py --file_name '../example_images/ex1.tif' --out_filter '[['axis_major_length', 0.2, 1]]'
-assert_no_stdout
-assert_exit_code 0
-
-run test_blob_main9 python ../blob_main.py --file_name '../example_images/ex1.tif' --best_filter '[['roughness_surface', 'min', 1]]'
 assert_no_stdout
 assert_exit_code 0
 
